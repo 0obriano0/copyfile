a1dcbcbc717e3e76be118dcefe264a33580899e9
0obriano0 <37872600+0obriano0@users.noreply.github.com>
2019-01-17 23:14:08 +0800

    createvc

---
+++ vc++/server/server/MyForm.cpp
@@ -0,0 +1,21 @@
+#include "MyForm.h"
+#include <omp.h>
+
+using namespace server;
+
+[STAThreadAttribute]
+
+int main(array<System::String ^> ^args) {
+	omp_set_nested(1);//打開openmp 巢狀執行緒 參考資料 https://www.phototalks.idv.tw/academic/?p=1997 
+
+	//true == 1, false == 0
+	// 建立任何控制項之前，先啟用 Windows XP 視覺化效果
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	// 建立主視窗並執行
+	Application::Run(gcnew MyForm());
+
+	//結束程式執行，並回傳 0 代表程式執行正常無異狀。
+	return 0;
+}
